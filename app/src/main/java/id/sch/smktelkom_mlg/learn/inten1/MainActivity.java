d111182372fe954acad03de16c353a4fe5c132a9
Norton <norton.nra27@gmail.com>
2016-11-03 13:14:49 +0700

    onBackPressed Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/inten1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/inten1/MainActivity.java
@@ -1,14 +1,53 @@
 package id.sch.smktelkom_mlg.learn.inten1;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NAMA = "Nama";
+    public static final String NAMA = NAMA;
+    public static final String NAME = NAMA;
+    public static final String UMUR = "Umur";
+    public static final String UMUR = UMUR;
+    EditText etnama;
+    EditText etumur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        etnama = (EditText) findViewById(R.id.editTextNama);
+        etumur = (EditText) findViewById(R.id.editTextUmur);
+
+        setTitle("Input");
+
+        findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
+                                                               @Override
+                                                               public void onClick(View v) {
+                                                                   goHasil();
+                                                               }
+                                                           }
+        );
+    }
+
+    private void goHasil() {
+        String nama = etnama.getText().toString();
+        String umurStr = etumur.getText().toString();
+
+        int umur = umurStr.isEmpty() ? 0 : Integer.parseInt(umurStr);
+
+        Intent intent = new Intent(MainActivity.this, HasilActivity.class);
+        intent.putExtra(NAME, nama);
+        intent.putExtra(UMUR, umur);
+
+
+        startActivity(intent);
     }
 }
